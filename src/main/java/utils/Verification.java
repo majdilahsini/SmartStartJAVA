3dfd67dd360b84c5e6cf79f87da7bf9acf48e4aa
Majdi Lahsini <majdi.lahsini@esprit.tn>
2019-10-23 21:28:13 +0200

    ajout classe verification sous package utils

---
+++ src/main/java/utils/Verification.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import Connection.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import services.ServiceOffre;
+
+/**
+ *
+ * @author Sadbo
+ */
+public class Verification {
+    
+    Connection c = DBConnection.getInstance().getConnection();
+    PreparedStatement ps;
+    Statement st;
+    ResultSet res = null;
+    
+    public Verification () {
+        try {
+            st = c.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(Verification.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }     
+    
+    public int siUsernameExiste(String s) {
+        
+        String r = "SELECT COUNT(*) FROM `users` WHERE `username`= ?";
+        int a = -1;
+        
+        try {
+            ps = c.prepareStatement(r);
+            ps.setString(1, s);
+            res = ps.executeQuery();
+            
+            if (res.next()) {
+                
+                //System.out.println(res.getInt(1));
+                
+                a = res.getInt(1);
+            }
+                
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Verification.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return a;
+
+}
+}
