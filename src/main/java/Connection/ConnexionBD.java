d169c1542e2353eb81b13337ff6f1c06897ee2bc
firos <firos@DESKTOP-8FM8SQ4>
2019-10-24 10:47:03 +0100

    cndbfiras

---
+++ src/main/java/Connection/ConnexionBD.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author firos
+ */
+public class ConnexionBD {
+    String url="jdbc:mysql://localhost:3306/smart_start";
+    String login="root";
+    String password="";
+    private Connection c;
+    private static ConnexionBD instance;
+   
+    private ConnexionBD() {
+        
+          try {
+              c = DriverManager.getConnection(url, login,
+                      password);
+              System.out.println("Connexion etablie");
+          } catch (SQLException ex) {
+              Logger.getLogger(ConnexionBD.class.getName()).log(Level.SEVERE, null, ex);
+          }
+    }
+    
+    public static ConnexionBD getInstanceConnexionBD() {
+        if (instance == null)
+            instance = new ConnexionBD();
+            return  instance;
+    }
+    public  Connection getConnection() {
+            return  c;
+    }
+}
