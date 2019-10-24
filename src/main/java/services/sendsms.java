0b9c267c9e90f05e6330f1fa0994d91e0d3743f2
firos <firos@DESKTOP-8FM8SQ4>
2019-10-24 23:04:17 +0100

    apisms

--- src/main/java/services/sendsms.java
+++ src/main/java/services/sendsms.java
@@ -7,14 +7,14 @@ import com.twilio.type.PhoneNumber;
 class Example {
     // Find your Account Sid and Token at twilio.com/console
     // DANGER! This is insecure. See http://twil.io/secure
-    public static final String ACCOUNT_SID = "AC9ab48ebae0622c629adccf761e1c593e";
-    public static final String AUTH_TOKEN = "ac147608d2fc4db2bfebf619d9db1c06";
+    public static final String ACCOUNT_SID = "PNe2bcb6897c840db06fc5989e0ebd7ce0";
+    public static final String AUTH_TOKEN = "665ec9681f5f6eb6dafaba85b4f36e6a";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                new com.twilio.type.PhoneNumber("+12244354506"),
                 new com.twilio.type.PhoneNumber("+21625695185"),
+                new com.twilio.type.PhoneNumber("+12244354506"),
                 "Hi there!")
             .create();
 
