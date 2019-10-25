520d12eff6ade19acc2eac4861b43730539c74ff
Majdi Lahsini <majdi.lahsini@esprit.tn>
2019-10-25 12:01:41 +0200

    espace entreprise menu + controller

---
+++ src/main/java/MainWindowEntrepriseController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sadbo
+ */
+public class MainWindowEntrepriseController implements Initializable {
+
+    @FXML
+    private JFXButton deconnect2;
+    @FXML
+    private TitledPane monespace;
+    @FXML
+    private JFXButton deconnect1;
+    @FXML
+    private JFXButton deconnect11;
+    @FXML
+    private JFXButton deconnect111;
+    @FXML
+    private JFXButton deconnect1111;
+    @FXML
+    private JFXButton deconnect11111;
+    @FXML
+    private TitledPane offrebtn;
+    @FXML
+    private JFXButton deconnect121;
+    @FXML
+    private JFXButton deconnect1211;
+    @FXML
+    private JFXButton deconnect1212;
+    @FXML
+    private JFXButton deconnect1213;
+    @FXML
+    private JFXButton deconnect12;
+    @FXML
+    private BorderPane borderpane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void returntologin(ActionEvent event) {
+    }
+
+    @FXML
+    private void offrebtnAction(MouseEvent event) {
+    }
+    
+}
