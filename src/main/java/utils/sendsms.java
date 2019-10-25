e16232b405f7332233d66a185fbcbbf312428e2d
Majdi Lahsini <majdi.lahsini@esprit.tn>
2019-10-25 12:59:39 +0200

    update (unpacking problem)

--- src/main/java/utils/sendsms.java
+++ src/main/java/utils/sendsms.java
@@ -1,8 +1,8 @@
 package utils;
 
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
+//import com.twilio.Twilio;
+//import com.twilio.rest.api.v2010.account.Message;
+//import com.twilio.type.PhoneNumber;
 
 class Example {
     // Find your Account Sid and Token at twilio.com/console
@@ -11,13 +11,13 @@ class Example {
     public static final String AUTH_TOKEN = "665ec9681f5f6eb6dafaba85b4f36e6a";
 
     public static void main(String[] args) {
-       Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+       /*Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                 new com.twilio.type.PhoneNumber("+21625695185"),
                 new com.twilio.type.PhoneNumber("+12244354506"),
                 "Hi there!")
             .create();
 
-        System.out.println(message.getSid());
+        System.out.println(message.getSid());*/
     }
 }
