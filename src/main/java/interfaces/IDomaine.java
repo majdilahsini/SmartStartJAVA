9d5686a2f2f5128d75136612f5c67783650e640f
racem-cherni <racem.cherni@esprit.tn>
2019-10-25 16:23:19 +0200

    interface service domaine

---
+++ src/main/java/interfaces/IDomaine.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import entities.Formation;
+
+/**
+ *
+ * @author lenovo
+ */
+public interface IDomaine {
+   public int getDomaineByID(String nom);
+}
