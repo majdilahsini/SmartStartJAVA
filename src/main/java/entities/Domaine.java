710c1d248f03742c3923ec6004ef0cdcc878c627
racem-cherni <racem.cherni@esprit.tn>
2019-10-25 16:15:22 +0200

    entitie Domaine ajouté

---
+++ src/main/java/entities/Domaine.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Domaine {
+    
+    int id ;
+    String nom_domaine;
+
+    public Domaine(int id, String nom_domaine) {
+        this.id = id;
+        this.nom_domaine = nom_domaine;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom_domaine() {
+        return nom_domaine;
+    }
+
+    public void setNom_domaine(String nom_domaine) {
+        this.nom_domaine = nom_domaine;
+    }
+
+}
