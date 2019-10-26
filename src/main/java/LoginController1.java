00d81b666b366a535c8158edec8da2bd6a80bae5
firos <firos@DESKTOP-8FM8SQ4>
2019-10-26 13:35:53 +0100

    logincrtrler

--- src/main/java/LoginController1.java
+++ src/main/java/LoginController1.java
@@ -8,20 +8,26 @@
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import entities.Session;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import services.loginservice;
 
 /**
  * FXML Controller class
@@ -30,6 +36,10 @@ import javafx.stage.Stage;
  */
 public class LoginController1 implements Initializable {
 
+    @FXML
+    private Label error;
+    @FXML
+    private ImageView verifIcon;
     @FXML
     private JFXButton loginbtn;
     @FXML
@@ -48,17 +58,119 @@ public class LoginController1 implements Initializable {
     }    
 
     @FXML
-    private void loginbtnAction(ActionEvent event) throws IOException {
+    private void loginbtnAction(ActionEvent event) throws IOException, SQLException {
         
-        Stage stage;
-        Parent signUpPage = FXMLLoader.load(getClass().getResource("/fxml/MainWindowEntreprise.fxml"));
-        Scene scene = new Scene(signUpPage);
-        stage = (Stage)loginbtn.getScene().getWindow();
-        stage.hide();
-        stage.setScene(scene);
-        stage.show();
+     if (loginField.getText().isEmpty()||pwdField.getText().isEmpty()){
+               showAlert(Alert.AlertType.ERROR, pwdField.getScene().getWindow(), 
+    "Form Error!", "Veuillez remplir tous les champs!");
+         
+         }
+         loginservice c=new loginservice();
+            String s=c.validerLogin(loginField.getText(),pwdField.getText());
+         
+ 
+         // validation.setText( s);
+        if(s.equals("utilisateur n'existe pas ")){
+                showAlert(Alert.AlertType.ERROR, pwdField.getScene().getWindow(), 
+    "Form Error!", "utilisateur n'existepas");
+        }
+        else if(s.equals("utilisateur correct ")){
+                
+        
+          int p=c.typeUsernumero(loginField.getText());
+            Session.id=c.getIdUtilisateur(loginField.getText(),pwdField.getText());
+            System.out.println(p);
+           // System.out.println(Session.id_utilisateur);
+            if (p==1){
+                            FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/MainWindowUtilisateur.fxml"));
+            Parent root =loader.load();
+            Scene tableViewScene = new Scene(root);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.close();
+            window.setScene(tableViewScene);
+           window.setMaximized(true);
+            window.show();
+
+            }
+          }
+           /*  if (p==0){
+                           // FXMLLoader loader2=new FXMLLoader(getClass().getResource("/View/AllUserNewController.fxml"));
+         FXMLLoader loader2=new FXMLLoader(getClass().getResource("/View/Admin.fxml"));
+                           
+                           Parent root =loader2.load();
+
+          
+            passwordId.getScene().setRoot(root);
+      
+            }
+              //pharmacien
+             if (p==2){
+                            FXMLLoader loader2=new FXMLLoader(getClass().getResource("/View/.fxml"));
+            Parent root =loader2.load();
+
+          
+            passwordId.getScene().setRoot(root);
+      
+            }
+               //infermier
+             if (p==3){
+                            FXMLLoader loader2=new FXMLLoader(getClass().getResource("/View/.fxml"));
+            Parent root =loader2.load();
+
+          
+            passwordId.getScene().setRoot(root);
+      
+            }
+               //Responsable organisation
+             if (p==4){
+                            FXMLLoader loader2=new FXMLLoader(getClass().getResource("/View/.fxml"));
+            Parent root =loader2.load();
+
+          
+            passwordId.getScene().setRoot(root);
+      
+            }
+                //Simple user
+             if (p==5){
+                            FXMLLoader loader2=new FXMLLoader(getClass().getResource("/View/.fxml"));
+            Parent root =loader2.load();
+
+          
+            passwordId.getScene().setRoot(root);
+      
+            }
+
+            
+          }
+          else 
+          {
+            showAlert(Alert.AlertType.ERROR, passwordId.getScene().getWindow(), 
+    "Form Error!", "utilisateur incorrect!");
+              
+            
+            }
+
+        
         
     }
+
+    private void showAlert(Alert.AlertType alertType, Window window, String form_Error, String veuillez_remplir_tous_les_champs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+ */
+    
+}
+    @FXML
+     private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+    Alert alert = new Alert(alertType);
+    alert.setTitle(title);
+    alert.setHeaderText(null);
+    alert.setContentText(message);
+    alert.initOwner(owner);
+    alert.show();
+}
+        
+    
 
     @FXML
     private void verifentry(KeyEvent event) {
