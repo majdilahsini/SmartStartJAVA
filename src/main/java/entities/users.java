a1351c384f28d561f0a8c6061742cfdc7f604e31
firos <firos@DESKTOP-8FM8SQ4>
2019-10-26 21:28:19 +0100

    usersddddd

---
+++ src/main/java/entities/users.java
@@ -0,0 +1,181 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author firos
+ */
+public class users {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String fullname;
+    private String tel;
+    private String adresse;
+    private String role;
+
+
+    public users(String username, String password, String email, String fullname, String tel, String adresse, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullname = fullname;
+        this.tel = tel;
+        this.adresse = adresse;
+        this.role = role;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+ public users() {
+    }
+    public users(int id, String username, String password, String email, String tel, String adresse, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.tel = tel;
+        this.adresse = adresse;
+        this.role = role;
+    }
+
+    public users(String username, String password, String email, String tel, String adresse, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.tel = tel;
+        this.adresse = adresse;
+        this.role = role;
+    }
+
+    public users(String username, String password, String email, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public users(String email, String tel, String adresse) {
+        this.email = email;
+        this.tel = tel;
+        this.adresse = adresse;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "users{" + "id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + ", tel=" + tel + ", adresse=" + adresse + ", role=" + role + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final users other = (users) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.tel, other.tel)) {
+            return false;
+        }
+        if (!Objects.equals(this.adresse, other.adresse)) {
+            return false;
+        }
+        if (!Objects.equals(this.role, other.role)) {
+            return false;
+        }
+        return true;
+    }
+
+   
+    
+}
---
+++ src/main/java/services/usersService.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.ConnexionBD;
+import entities.users;
+/**
+ *
+ * @author firos
+ */
+public class usersService {
+    Connection c= ConnexionBD.getInstanceConnexionBD().getConnection();
+    
+    public String ajouterUtilisateur(users p){
+        String m="non valide";
+       try {
+             PreparedStatement pt = c.prepareStatement("select * from users where username =? and password=?");
+            pt.setString(1, p.getUsername());
+             pt.setString(2, p.getPassword());
+            ResultSet rs=pt.executeQuery();
+  if(rs.next())
+  { 
+      System.out.println( "utilisateur existe ");
+   
+  
+  }
+  else
+  {Statement st=c.createStatement();//qui va assurer la connexion entre sql et java 
+         
+       String req="insert into users(username,password,email,fullname,tel,adresse,role)  values('"+ p.getUsername()+"','"+p.getPassword()+"','"+p.getEmail()+"','"+p.getFullname()+"','"+p.getTel()+"','"+p.getAdresse()+"','"+p.getRole()+"')";
+       st.executeUpdate(req);
+       m="vous ete inscrit";
+          
+  }
+       } catch (SQLException ex) {
+           Logger.getLogger(usersService.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       return m;
+    }
+    public void modifierUtilisateur (Integer id ,users u ){
+       try {
+           PreparedStatement pt =c.prepareStatement("update users set  username =?,password=?,email =?,fullname=?,adresse=?,tel=?,role=? where id_utilisateur=?");
+          pt.setString(1, u.getUsername());
+           pt.setString(2, u.getPassword());
+             pt.setString(3, u.getEmail());
+               pt.setString(4, u.getFullname());
+              
+               
+            pt.setString(5, u.getAdresse());
+            pt.setString(6, u.getTel());
+               pt.setString(7, u.getRole());
+             
+               pt.setInt(10,id);
+               
+             
+            pt.executeUpdate();
+       } catch (SQLException ex) {
+           Logger.getLogger(usersService.class.getName()).log(Level.SEVERE, null, ex);
+       }
+    
+    
+    }
+    public void modifierUtilisateurProfile (Integer id ,users u ){
+       try {
+           PreparedStatement pt =c.prepareStatement("update users set  username =?,email =?,fullname=?,adresse=?,tel=? where id=?");
+          pt.setString(1, u.getUsername());
+           pt.setString(2, u.getEmail());
+             pt.setString(3, u.getFullname());
+          
+              
+               
+            pt.setString(4, u.getAdresse());
+            pt.setString(5, u.getTel());
+            
+               pt.setInt(6,id);
+               
+             
+            pt.executeUpdate();
+       } catch (SQLException ex) {
+           Logger.getLogger(usersService.class.getName()).log(Level.SEVERE, null, ex);
+       }
+    
+    
+}
+    public boolean  SupprimerUser (int id ){
+       boolean x=false;
+        try {
+           PreparedStatement pt =c.prepareStatement("delete from users where id=?");
+           pt.setInt(1, id );
+           pt.executeUpdate(); 
+           x=true;
+       } catch (SQLException ex) {
+           Logger.getLogger(usersService.class.getName()).log(Level.SEVERE, null, ex);
+       }
+        return x;
+    }
+     public String getTel(String text) {
+          String t = null;
+         try {
+             PreparedStatement pt = c.prepareStatement("select tel from users  where users.email=?");
+             pt.setString(1, text );
+             ResultSet rs=pt.executeQuery();
+              while(rs.next()){
+            
+                      t=rs.getString("tel");
+              }
+         } catch (SQLException ex) {
+             Logger.getLogger(usersService.class.getName()).log(Level.SEVERE, null, ex);
+         }
+       
+         return t;
+    }
+     public String getRole(String email) {
+          String t = null;
+         try {
+             PreparedStatement pt = c.prepareStatement("select role from users  where users.email=?");
+             pt.setString(1, email );
+             ResultSet rs=pt.executeQuery();
+              while(rs.next()){
+            
+                      t=rs.getString("role");
+              }
+         } catch (SQLException ex) {
+             Logger.getLogger(usersService.class.getName()).log(Level.SEVERE, null, ex);
+         }
+       
+         return t;
+    }
+     public String  validerLogin(String username, String password  ) throws SQLException{
+      String m= null;
+       
+             PreparedStatement pt = c.prepareStatement("select count(*) as total from users where username=? and password=? ");
+             pt.setString(1,username );
+              pt.setString(2,password );
+                  
+             ResultSet rs=pt.executeQuery();
+             rs.next();
+             int l1=rs.getInt("total");
+             System.out.println("nbr" +l1);
+             rs.close();
+         // int  l= rs.getRow();
+          //int l =rs.getInt("total");
+          //  System.out.println(l1);
+          if (l1!=1){
+              m= "utilisateur n'existe pas ";
+          }
+            else  m=  "utilisateur correct ";
+         
+         return m;
+       
+  }
+}
