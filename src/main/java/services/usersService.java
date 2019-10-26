f613cf44830f234e200008ea48363ae7dbb4b482
firos <firos@DESKTOP-8FM8SQ4>
2019-10-27 00:34:04 +0100

    serviceuser

--- src/main/java/services/usersService.java
+++ src/main/java/services/usersService.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import utils.ConnexionBD;
+import Connection.ConnexionBD;
 import entities.users;
 /**
  *
