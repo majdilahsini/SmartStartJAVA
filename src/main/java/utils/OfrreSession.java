4aabf96b5f97b99bb87c33c3d41626a94d7a9f15
Majdi Lahsini <majdi.lahsini@esprit.tn>
2019-10-28 14:48:34 +0100

    ajout offreSession pour getIdOffre

---
+++ src/main/java/utils/OfrreSession.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Sadbo
+ */
+public class OfrreSession {
+    
+    private static int id;
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        OfrreSession.id = id;
+    }
+    
+    
+    
+}
